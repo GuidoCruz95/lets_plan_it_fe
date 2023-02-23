6dfa54603779414a935c55522b1468ced14f5238
Guido Cruz <guidocruz@Guidos-MacBook-Pro.local>
2023-02-23 12:30:03 -0400

    List views updated to get data from the server instead of json files

--- src/views/Events.js
+++ src/views/Events.js
@@ -1,7 +1,22 @@
-import events from "../data/events.json";
+import staticVariables from "../components/variables/resources";
+import React, { useState, useEffect } from 'react';
 import ItemsList from '../layouts/Items'
 
 function EventsList() {
+
+    const [events, setEvents] = useState([]);
+    useEffect(() => {
+        fetch(staticVariables['HOST'] + staticVariables['ENTRY_POINTS']['events'])
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setEvents(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
     let tittle = 'Lista de eventos registrados'
     let headers = ['No.', 'Nombre', 'Lugar', 'Fecha Inicio', 'Fecha Fin', 'Costo']
     let data_parsed = events.map((prop, key) => {
--- src/views/Members.js
+++ src/views/Members.js
@@ -1,7 +1,21 @@
-import members from "../data/members.json";
+import staticVariables from "../components/variables/resources";
+import React, { useState, useEffect } from 'react';
 import ItemsList from '../layouts/Items'
 
-function MembersList(){
+function MembersList() {
+    const [members, setMembers] = useState([]);
+    useEffect(() => {
+        fetch(staticVariables['HOST'] + staticVariables['ENTRY_POINTS']['people'])
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setMembers(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
     let tittle = 'Lista de miembros registrados'
     let headers = ['No.', 'Ci', 'Email', 'Name', 'LastName']
     let data_parsed = members.map((prop, key) => {
@@ -15,7 +29,7 @@ function MembersList(){
             </tr>
         );
     })
-    return ItemsList(tittle, headers, data_parsed, {'to': 'new-member', 'title': 'Registrar nuevo'})
+    return ItemsList(tittle, headers, data_parsed, { 'to': 'new-member', 'title': 'Registrar nuevo' })
 }
 
 export default MembersList;
